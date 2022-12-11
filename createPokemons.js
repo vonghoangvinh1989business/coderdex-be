6f96fdd9e0ca1495aef659df12faf100036caeb1
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2022-12-11 22:29:48 +0700

    setup code and create pokemon data for project

---
+++ createPokemons.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const csv = require("csvtojson");
+require("dotenv").config();
+const _ = require("lodash");
+
+let id = 0;
+const DOMAIN_NAME = process.env.DOMAIN_NAME;
+const PROTOCOL = process.env.PROTOCOL;
+const PORT = process.env.PORT;
+console.log(`DOMAIN_NAME is: ${DOMAIN_NAME}`);
+console.log(`PROTOCOL is: ${PROTOCOL}`);
+console.log(`PORT is: ${PORT}`);
+
+const createPokemonData = async () => {
+  let pokemonNewData = await csv().fromFile("pokemons.csv");
+
+  pokemonNewData = pokemonNewData
+    .map((pokemon) => {
+      // create id for each pokemon
+      id++;
+
+      const pokemonObject = {
+        id,
+        name: _.lowerCase(pokemon.Name),
+        types: [_.lowerCase(pokemon.Type1), _.lowerCase(pokemon.Type2)].filter(
+          (type) => type
+        ),
+        url: `${PROTOCOL}://${DOMAIN_NAME}:${PORT}/images/${id}.png`,
+      };
+      return pokemonObject;
+    })
+    .filter((pokemon) => fs.existsSync(`./public/images/${pokemon.id}.png`));
+
+  let pokemonData = JSON.parse(fs.readFileSync("pokemons.json"));
+  pokemonData.data = pokemonNewData;
+  pokemonData["totalPokemons"] = pokemonNewData.length;
+
+  fs.writeFileSync("pokemons.json", JSON.stringify(pokemonData));
+  console.log("---processing done---");
+};
+
+// testing
+createPokemonData();
